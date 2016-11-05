77fa7b3debcec4ef17816a9ae80dbabcb1f50fa2
Ryan Zhang <Ryan778@users.noreply.github.com>
2016-11-04 20:21:22 -0600

    Transitions

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function start(){
-	document.getElementById('intro_screen').style.display = 'none';
-	document.getElementById('frame_div').style.display = 'initial';
+	document.getElementById('intro_screen').style.opacity = '0';
+	setTimeout(function(){
+		document.getElementById('intro_screen').style.display = 'none';
+		document.getElementById('frame_div').style.display = 'initial';
+	}, 1000)
 }
