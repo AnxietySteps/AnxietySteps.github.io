58c2790eb1a3f1354d62a49288980b040c8de165
Ryan Zhang <Ryan778@users.noreply.github.com>
2016-11-04 20:30:39 -0600

    Smooth Transitions from Start to Frame

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function start(){
+	document.getElementById('p_readyToStart').style.opacity = '0';
 	document.getElementById('btn_start').style.opacity = '0';
 	document.getElementById('btn_start').style.pointer = 'default';
 	setTimeout(function(){
 		document.getElementById('intro_screen').style.display = 'none';
 		document.getElementById('frame_div').style.display = 'initial';
-	}, 1000)
+		setTimeout(function(){
+			document.getElementById('frame_div').style.opacity = '1';
+		}, 5);
+	}, 500)
 }
