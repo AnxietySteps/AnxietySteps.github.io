7f749f5fe7cd7cfe47248bde054c506af2cd6750
Ryan Zhang <Ryan778@users.noreply.github.com>
2016-11-05 08:23:01 -0600

    Better Fading

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
+"use strict";
+
 function start() {
-    "use strict";
-    document.getElementById('p_readyToStart').style.opacity = '0';
-    document.getElementById('btn_start').style.opacity = '0';
-    document.getElementById('btn_start').style.pointer = 'default';
-    setTimeout(function () {
-        document.getElementById('intro_screen').style.display = 'none';
-        document.getElementById('frame_div').style.display = 'initial';
-        setTimeout(function () {
-            document.getElementById('frame_div').style.opacity = '1';
-        }, 5);
-    }, 500);
+    $('intro_screen').fadeOut(500);
+    setTimeout(function(){
+        $('frame_div').fadeIn(500);
+    })
 }
