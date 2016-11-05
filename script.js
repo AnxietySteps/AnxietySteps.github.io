7317125e8069ca053109c00f892ec06e3e7a20f0
SkyrisBactera <davis.davalos.delosh@gmail.com>
2016-11-04 20:09:41 -0600

    Making code more readable

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function start() {
     document.getElementById('p_readyToStart').style.opacity = '0';
     document.getElementById('btn_start').style.opacity = '0';
     document.getElementById('btn_start').style.pointer = 'default';
-    setTimeout(function(){
+    setTimeout(function () {
         document.getElementById('intro_screen').style.display = 'none';
         document.getElementById('frame_div').style.display = 'initial';
-        setTimeout(function(){
+        setTimeout(function () {
             document.getElementById('frame_div').style.opacity = '1';
         }, 5);
-	}, 500);
+    }, 500);
 }
