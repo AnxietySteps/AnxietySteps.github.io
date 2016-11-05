e1ff3fcb8ac40fc6e957e46fbd98c7aadd945d6e
Ryan Zhang <Ryan778@users.noreply.github.com>
2016-11-05 10:49:58 -0600

    Smoother Counting

--- script-countToTen.js
+++ script-countToTen.js
@@ -5,15 +5,16 @@ function runAnimation(){
     ctn.style.fontSize = '150px';
     ctn.style.color = 'rgba(255,255,255,0)';
     setTimeout(function(){
-        ctn.style.transition = 'all 0s';
+        ctn.style.transition = 'color 0.1s';
         ctn.style.fontSize = '100px';
-        ctn.style.transition = 'all 0.1s';
         ctn.style.color = 'rgb(0,0,0)';
+        count --;
+        if(count >= 0){
+            ctn.innerHTML = count.toString();
+        }
     }, 900);
     if(count > 0){
         setTimeout(function(){
-            count --;
-            ctn.innerHTML = count.toString();
             runAnimation();
         }, 1000);
     }
