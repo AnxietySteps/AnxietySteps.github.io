906f8c000d4f1398f2ea4df2a24fa69d59c0c03a
SkyrisBactera <davis.davalos.delosh@gmail.com>
2016-11-04 19:57:28 -0600

    Fixing script style

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function start(){
-	document.getElementById('p_readyToStart').style.opacity = '0';
-	document.getElementById('btn_start').style.opacity = '0';
-	document.getElementById('btn_start').style.pointer = 'default';
+function start() {
+	document.getElementById('p_readyToStart').style.opacity = '0'
+	document.getElementById('btn_start').style.opacity = '0'
+	document.getElementById('btn_start').style.pointer = 'default'
 	setTimeout(function(){
-		document.getElementById('intro_screen').style.display = 'none';
-		document.getElementById('frame_div').style.display = 'initial';
+		document.getElementById('intro_screen').style.display = 'none'
+		document.getElementById('frame_div').style.display = 'initial'
 		setTimeout(function(){
-			document.getElementById('frame_div').style.opacity = '1';
-		}, 5);
+			document.getElementById('frame_div').style.opacity = '1'
+		}, 5)
 	}, 500)
 }
