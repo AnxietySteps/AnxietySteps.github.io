7b309d27f6881c0a9de7a6daae6ac97aa3a4f7dd
Ryan Zhang <Ryan778@users.noreply.github.com>
2016-11-05 08:30:31 -0600

    Revert breaking script

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-"use strict";
-
 function start() {
-    $('intro_screen').fadeOut(500);
-    setTimeout(function(){
-        $('frame_div').fadeIn(500);
-    })
+    "use strict";
+    document.getElementById('p_readyToStart').style.opacity = '0';
+    document.getElementById('btn_start').style.opacity = '0';
+    document.getElementById('btn_start').style.pointer = 'default';
+    setTimeout(function () {
+        document.getElementById('intro_screen').style.display = 'none';
+        document.getElementById('frame_div').style.display = 'initial';
+        setTimeout(function () {
+            document.getElementById('frame_div').style.opacity = '1';
+        }, 5);
+    }, 500);
 }
